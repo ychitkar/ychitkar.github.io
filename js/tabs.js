8079ec58778f0e858c7c8a358fda4c94c5ee4010
ychitkar <115811227+ychitkar@users.noreply.github.com>
2022-11-23 21:13:45 +0530

    Updated tabs for Awards

--- js/tabs.js
+++ js/tabs.js
@@ -1,10 +1,20 @@
 function switchTab(tab) {
-    var ex = document.querySelector('.experience');
+    var aw = document.querySelector('.award') 
+    ex = document.querySelector('.experience');
 
     var ext = document.querySelector('.ex-tab');
+    var awt = document.querySelector('.aw-tab');
 
     switch (tab) {
-        case 'ex':
+        case 'aw':
+            ex.style.display = 'none';
+            ext.className = 'tab ex-tab';
+            aw.style.display = 'block';
+            awt.className = 'tab aw-tab active';
+            break;
+         case 'ex':
+            aw.style.display = 'none';
+            awt.className = 'tab aw-tab';
             ex.style.display = 'block';
             ext.className = 'tab ex-tab active';
             break;
