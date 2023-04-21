2b25ffd5b421617c5697a675c8096ce571cc69ab
ychitkar <115811227+ychitkar@users.noreply.github.com>
2023-04-21 19:08:23 +0530

    Added Course tab

--- js/tabs.js
+++ js/tabs.js
@@ -1,22 +1,36 @@
 function switchTab(tab) {
     var aw = document.querySelector('.award') 
-    ex = document.querySelector('.experience');
+    ex = document.querySelector('.experience'),
+    co = document.querySelector('.course');
 
     var ext = document.querySelector('.ex-tab');
     var awt = document.querySelector('.aw-tab');
+    var cot = document.querySelector('.co-tab');
 
     switch (tab) {
         case 'aw':
             ex.style.display = 'none';
             ext.className = 'tab ex-tab';
+            co.style.display = 'none';
+            cot.className = 'tab co-tab';
             aw.style.display = 'block';
             awt.className = 'tab aw-tab active';
             break;
          case 'ex':
             aw.style.display = 'none';
             awt.className = 'tab aw-tab';
+            co.style.display = 'none';
+            cot.className = 'tab co-tab';
             ex.style.display = 'block';
             ext.className = 'tab ex-tab active';
+            break;
+        case 'co':
+            ex.style.display = 'none';
+            ext.className = 'tab ex-tab';
+            aw.style.display = 'none';
+            awt.className = 'tab aw-tab';
+            co.style.display = 'block';
+            cot.className = 'tab co-tab active';
             break;
     }
 }
